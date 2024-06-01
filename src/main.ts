dc05bf60fed1792c5d2d04cafba1811b9b7a98a0
Tramaine Gillus <1573935+tgillus@users.noreply.github.com>
2024-06-01 16:17:43 -0400

    chore: clean up

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,7 @@ import './app.css'
 import App from './App.svelte'
 
 const app = new App({
+  // biome-ignore lint/style/noNonNullAssertion: <explanation>
   target: document.getElementById('app')!,
 })
 
